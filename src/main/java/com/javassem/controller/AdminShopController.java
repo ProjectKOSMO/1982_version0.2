a07d9691f943c388c201a5256d0533bcb75669ca
ProjectKOSMO <93956446+ProjectKOSMO@users.noreply.github.com>
2021-12-29 10:31:22 +0900

    Update AdminShopController.java

--- src/main/java/com/javassem/controller/AdminShopController.java
+++ src/main/java/com/javassem/controller/AdminShopController.java
@@ -7,8 +7,14 @@ import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-private ShopService shopService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping({"admin"})
+public class AdminShopController {
+	
+  @Autowired
+  private ShopService shopService;
 
   @RequestMapping({"storeClose.do"})
   public void select(HashMap vo, Model m) {
